8ee7315f9f67aa037ba95476d8360e5c249d5092
liuruichao <liuruichao555@126.com>
2017-03-24 17:48:11 +0800

    add Test.java

---
+++ config-client/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Test
+ *
+ * @author liuruichao
+ * Created on 2017/3/24 17:39
+ */
+public class Test {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("dfaf");
+    }
+}
