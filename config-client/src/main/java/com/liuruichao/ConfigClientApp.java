0cec9ea301457859f7bfdd8597d3e3f8df111d26
liuruichao <liuruichao555@126.com>
2017-04-22 17:48:27 +0800

    spring cloud config done

---
+++ config-client/src/main/java/com/liuruichao/ConfigClientApp.java
@@ -0,0 +1,36 @@
+package com.liuruichao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ConfigClientApp
+ *
+ * @author liuruichao
+ * Created on 2017/4/22 14:47
+ */
+@SpringBootApplication
+@Configuration
+@RestController
+@RequestMapping("")
+public class ConfigClientApp {
+    /*@Value("${redis.host}")
+    private String redisHost;*/
+
+    @Value("${bar}")
+    private String bar;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return bar;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApp.class, args);
+    }
+}
--- config-server/README.md
+++ config-server/README.md
@@ -4,3 +4,11 @@
        application: http://localhost:8080/configserver/application
     2. refresh
        http://localhost:8080/refresh
+       
+## 资源规则
+
+| Path             | Description  |
+|------------------|--------------|
+| /{app}/{profile} | Configuration data for app in Spring profile (comma-separated).|
+| /{app}/{profile}/{label} | Add a git label |
+| /{app}/{profiels}{label}/{path} | An environment-specific plain text config file (at "path") |
