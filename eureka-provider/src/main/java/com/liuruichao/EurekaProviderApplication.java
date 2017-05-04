efd3326d837f4ec902f6eeed5a3d534861d4f906
liuruichao <liuruichao555@126.com>
2017-05-04 12:20:36 +0800

    del useless import

--- eureka-provider/src/main/java/com/liuruichao/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/com/liuruichao/EurekaProviderApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
