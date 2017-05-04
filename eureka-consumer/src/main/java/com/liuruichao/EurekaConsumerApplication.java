183d4ed107161739ae6eed4e8f0fa019e6b4a74b
liuruichao <liuruichao555@126.com>
2017-05-04 12:20:27 +0800

    del useless import

--- eureka-consumer/src/main/java/com/liuruichao/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/liuruichao/EurekaConsumerApplication.java
@@ -3,12 +3,10 @@ package com.liuruichao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
